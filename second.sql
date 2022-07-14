aabc2c72d0dbee1d877d943968a1430cad49ae28
textsfromyourex <skgaming7869@gmail.com>
2022-07-14 08:13:33 +0530

    Basics: Here we learn a few more commands to write the query. 	1)Access null values and update them 	2)Update the contents of the table 	3)Delete table columns 	4)Alter table columns TO DO: Fill all null values

---
+++ second.sql
@@ -0,0 +1,51 @@
+/*
+SQL NULL VALUES
+SQL UPDATE STATEMENT
+SQL DELETE STATEMENT
+SQL ALTER TABLE
+	add column in existing column
+    modify/alter a column
+    alter table drop column
+*/
+#CREATE DATABASE customer;
+USE customer;
+
+CREATE TABLE customer_info(
+id INTEGER AUTO_INCREMENT,
+first_name VARCHAR(25),
+last_name VARCHAR(25),
+salary INTEGER,
+PRIMARY KEY(id)
+);
+
+#INSTERT STUFF INTO THE TABLE
+INSERT INTO customer_info(first_name,last_name,salary)
+VALUES('SUYOG','KURLEKAR',50000),
+	  ('SWAYAM','KURLEKAR',60000),
+      ('SWARAJ','M',70000),
+      ('VIRAT','KARANJKAR',40000),
+      ('KRISHNA','KUMAR',NULL);
+
+
+
+# SQL UPDATE STATEMENT TO REPLACE NULL VALUES
+
+UPDATE customer_info SET salary=50000 WHERE id=5;   
+
+# DELETE STUFF FROM THE TABLE
+
+DELETE FROM customer_info WHERE id=5;
+      
+SELECT * FROM customer_info;
+
+# SQL ALTER TABLE
+# ADD COLUMN IN EXISTING TABLE
+
+ALTER TABLE customer_info ADD email VARCHAR(25);
+ALTER TABLE customer_info ADD dob DATE;
+
+# ALTER TABLE MODIFY COLUMN
+ALTER TABLE customer_info MODIFY dob YEAR;
+
+#ALTER TABLE TO DROP COLUMN
+ALTER TABLE customer_info DROP COLUMN email;
